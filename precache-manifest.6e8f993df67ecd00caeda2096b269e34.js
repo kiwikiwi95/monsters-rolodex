e2597fb5aa81c6ed4e71b68585f9b4039902203a
Kiwi <jc1887@rutgers.edu>
2020-05-04 11:32:02 -0400

    Updates

---
+++ precache-manifest.6e8f993df67ecd00caeda2096b269e34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd64c56f91f975d4a6b6440cc83c589",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "84236c26bf27e5c524ae",
+    "url": "/monsters-rolodex/static/css/main.47f64396.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84236c26bf27e5c524ae",
+    "url": "/monsters-rolodex/static/js/main.73566176.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
