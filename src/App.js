2e519d9cc76f5072cdbb64697494d4dcf3bfcdd8
Kiwi <jc1887@rutgers.edu>
2021-04-16 09:30:54 -0400

    for gitflow

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { CardList } from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component";
 
+
 class App extends Component {
   constructor() {
     super();
